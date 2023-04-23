15f921e629ada74d9aeb130d620da2f8a1bf4748
Evans K F Chikuni <vance04real@gmail.com>
2023-04-23 12:33:49 +0200

    Initial commit

---
+++ src/main/java/za/co/discovery/assignment/evanschikuni/EvansChikuniApplication.java
@@ -0,0 +1,13 @@
+package za.co.discovery.assignment.evanschikuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvansChikuniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvansChikuniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/co/discovery/assignment/evanschikuni/EvansChikuniApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.discovery.assignment.evanschikuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvansChikuniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
